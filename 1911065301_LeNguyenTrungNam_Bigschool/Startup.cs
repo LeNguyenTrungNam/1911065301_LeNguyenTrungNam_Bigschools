4016c5ab89ccc5396840334bc4d63061050e6abb
LeNguyenTrungNam <lenguyentrungnam22@gmail.com>
2022-03-25 10:25:26 +0700

    Add project files.

---
+++ 1911065301_LeNguyenTrungNam_Bigschool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1911065301_LeNguyenTrungNam_Bigschool.Startup))]
+namespace _1911065301_LeNguyenTrungNam_Bigschool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
